d4cdaa4029944df7219c9818a53d0452b825ecd5
Nark <sooryarox2004@gmail.com>
2024-01-02 00:26:24 +0200

    init

---
+++ notesBackend/models/note.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const { MONGODB_URI } = require("../utils/config");
+
+mongoose.set("strictQuery", false);
+
+console.log("connecting to", MONGODB_URI);
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  important: Boolean,
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
