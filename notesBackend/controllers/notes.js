613eba350d56300510eb8a8cdcbdeebead9d3c83
Nark <sooryarox2004@gmail.com>
2024-01-04 01:30:58 +0200

    4c

--- notesBackend/controllers/notes.js
+++ notesBackend/controllers/notes.js
@@ -1,20 +1,28 @@
 const notesRouter = require('express').Router()
+
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id,
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concact(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 
@@ -40,7 +48,9 @@ notesRouter.put('/:id', async (request, response) => {
     important: body.important,
   }
 
-  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+  })
   response.json(updatedNote)
 })
 
--- notesBackend/tests/note_api.test.js
+++ notesBackend/tests/note_api.test.js
@@ -133,7 +133,7 @@ describe('when there is initially one user in db', () => {
     await User.deleteMany({})
 
     const passwordHash = await bcrypt.hash('heycoolnicepasswordbro', 10)
-    const user = new User({ username: 'nice', passwordHash })
+    const user = new User({ username: 'root', passwordHash })
 
     await user.save()
   })
@@ -142,7 +142,7 @@ describe('when there is initially one user in db', () => {
     const usersAtStart = await helper.usersInDb()
 
     const newUser = {
-      username: 'nicer',
+      username: 'nice',
       name: 'nicer nice',
       password: 'trustmethisisagoodpassword',
     }
@@ -159,6 +159,28 @@ describe('when there is initially one user in db', () => {
 
     const usernames = usersAtEnd.map((u) => u.username)
     expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper status code and message if username is already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'cool beans',
+      passowrd: 'thisoneisareallycoolpassword'
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('expected `username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+
+    expect(usersAtEnd).toEqual(usersAtStart)
   })
 })
 
