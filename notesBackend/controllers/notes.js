33d354e8e0740fbe1b4f34f4004ea253da496b53
Nark <sooryarox2004@gmail.com>
2024-01-05 02:27:55 +0200

    minor .post optimizationi

--- notesBackend/controllers/notes.js
+++ notesBackend/controllers/notes.js
@@ -17,12 +17,13 @@ notesRouter.get('/', async (request, response) => {
 })
 
 notesRouter.post('/', async (request, response) => {
-  const body = request.body
   const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
 
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
+
+  const body = request.body
   const user = await User.findById(decodedToken.id)
 
   const note = new Note({
